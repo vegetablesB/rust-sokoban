e807d952f0116ef1ab89d2cab8efa808979e71f4
vegetablesB <caoning3708@gmail.com>
2024-06-08 17:45:08 -0400

    feat: add batch rendering

--- src/systems/rendering_system.rs
+++ src/systems/rendering_system.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
+
 use ggez::{Context, graphics};
 use ggez::glam::Vec2;
-use ggez::graphics::{Canvas, Color, DrawParam, Image, TextFragment};
+use ggez::graphics::{Canvas, Color, DrawParam, Image, InstanceArray, TextFragment};
+use itertools::Itertools;
 use specs::{Join, Read, ReadStorage, System};
 
 use crate::components::*;
@@ -29,20 +32,36 @@ impl<'a> System<'a> for RenderingSystem<'a> {
         let mut rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
         rendering_data.sort_by_key(|&k| k.0.z);
 
-        // Iterate through all pairs of positions & renderables, load the image
-        // and draw it at the specified position.
+        // Group renderables by their image path
+        let mut rendering_batches: HashMap<u8, HashMap<String, Vec<DrawParam>>> = HashMap::new();
         for (position, renderable) in rendering_data.iter() {
-            // Load the image
-            // let image = Image::new(self.context, renderable.path.clone()).expect("expected image");
-            let image =
-                Image::from_path(self.context, renderable.path.clone()).expect("expected image");
-            let x = position.x as f32 * TILE_WIDTH;
-            let y = position.y as f32 * TILE_WIDTH;
-
-            // draw
-            let draw_params = DrawParam::new().dest(Vec2::new(x, y));
-            canvas.draw(&image, draw_params);
+            rendering_batches
+                .entry(position.z)
+                .or_insert_with(HashMap::new)
+                .entry(renderable.path.clone())
+                .or_insert_with(Vec::new)
+                .push(DrawParam::new().dest(Vec2::new(
+                    position.x as f32 * TILE_WIDTH,
+                    position.y as f32 * TILE_WIDTH,
+                )));
         }
+
+        // Sort the keys of renderable_groups to ensure consistent order
+        // without .sorted_by(|a, b| a.0.cmp(b.0)) will cause image flickering, use this
+        // sorted_by from itertools to ensure consistent rendering order
+
+        // Draw each group
+        for (_z, group) in rendering_batches.iter().sorted_by(|a, b| a.0.cmp(b.0)) {
+            for (path, draw_param) in group {
+                let image = Image::from_path(self.context, path).expect("expected image");
+                let mut instance_array = InstanceArray::new(self.context, image);
+                for param in draw_param.iter() {
+                    instance_array.push(param.clone());
+                }
+                canvas.draw(&instance_array, DrawParam::default());
+            }
+        }
+
         // Render any text
         self.draw_text(&mut canvas, gameplay.state.to_string(), 525.0, 80.0, 44.0);
         self.draw_text(
